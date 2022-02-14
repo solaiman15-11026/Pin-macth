ea0a0ebce137118183dfa93af9908ff9c672dec2
maruf sheik <samimunna49@gmail.com>
2022-02-14 12:02:45 +0600

    donee

--- s.js
+++ s.js
@@ -24,8 +24,24 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
     } else {
 
         const preCalNumber = caltulatorInput.value;
-        const total = preCalNumber + number;
-        caltulatorInput.value = total;
+        const againCalNumber = preCalNumber + number;
+        caltulatorInput.value = againCalNumber;
     }
 
 })
+
+function verify() {
+    const pin = document.getElementById('diplay-pin').value;
+    const typeInput = document.getElementById('type-number').value;
+    const pinMacth = document.getElementById('notify-sucuss');
+    const pinNotMacth = document.getElementById('notify-fail');
+    if (pin == typeInput) {
+
+        pinMacth.style.display = 'block';
+        pinNotMacth.style.display = 'none';
+    } else {
+
+        pinNotMacth.style.display = 'block';
+        pinMacth.style.display = 'none';
+    }
+}
